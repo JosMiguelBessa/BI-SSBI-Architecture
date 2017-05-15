e9f41b28f4415c2b5b01f85f87837c56822f539f
José Bessa <jmiguelbessa16@gmail.com>
2017-05-15 16:23:34 +0100

    Script de Consolidação de Tickets

--- Scripts-Consolidacao/Script_Clean_Tickets.cs
+++ Scripts-Consolidacao/Script_Clean_Tickets.cs
@@ -16,35 +16,35 @@ namespace Remove_Comma_Tickets_TXT
     {
         public Form1()
         {
-			//Método instanciado por defeito utilizado para inicializar a interface gráfica da aplicação
+	    //Método instanciado por defeito utilizado para inicializar a interface gráfica da aplicação
             InitializeComponent();
-			//Esconder progress bar quando arranca o programa
+	    //Esconder progress bar quando arranca o programa
             progressBarClean.Visible = false;
-			//Valor mínimo da progress bar
+	    //Valor mínimo da progress bar
             progressBarClean.Minimum = 0;
-			//Valor adicionado à progress bar em cada iteração
+	    //Valor adicionado à progress bar em cada iteração
             progressBarClean.Step = 1;
-			//Esconder label das etapas quando arranca o programa
-			label_step.Visible = false;
+	    //Esconder label das etapas quando arranca o programa
+	    label_step.Visible = false;
         }
-		//Codificação do Botão que efetua a consolidação e transformação dos eventos do ficheiro
+	//Codificação do Botão que efetua a consolidação e transformação dos eventos do ficheiro
         private void Button_Limpar_Click(object sender, EventArgs e)
         {
-			//Variável que irá guardar o caminho para o ficheiro que armazena o resultado do processamento
+	    //Variável que irá guardar o caminho para o ficheiro que armazena o resultado do processamento
             string savefilename = "";
-			//Variável com o valor máximo da progress bar (que equivale ao número de evento a serem processados)
+	    //Variável com o valor máximo da progress bar (que equivale ao número de evento a serem processados)
             int count_max_prog_bar = 0;
 
-			//Verificar se foi preenchido o caminho para um ficheiro
+	    //Verificar se foi preenchido o caminho para um ficheiro
             if (string.IsNullOrEmpty(textBox_Ficheiro.Text))
             {
-				//Apresentar uma mensagem caso não tenha sido preenchido o caminho para um ficheiro
+		//Apresentar uma mensagem caso não tenha sido preenchido o caminho para um ficheiro
                 MessageBox.Show("Path não foi preenchida!!!\nPreencha primeiro a path!!");
             }
-			//Caso tenha sido preenchido o caminho para o ficheiro
+	    //Caso tenha sido preenchido o caminho para o ficheiro
             else
             {
-				//Verificar se o ficheiro existe
+		//Verificar se o ficheiro existe
                 if (File.Exists(textBox_Ficheiro.Text))
                 {
                     //Adicionar onde guardar ficheiro
@@ -52,24 +52,24 @@ namespace Remove_Comma_Tickets_TXT
                     saveFileDialog1.InitialDirectory = Convert.ToString(Environment.SpecialFolder.Desktop);
                     saveFileDialog1.Filter = "Text File (.txt) | *.txt";
                     saveFileDialog1.FilterIndex = 1;
-					//Verificar se é carregado no botão OK. Se sim, guarda o ficheiro na localização escolhida
+		    //Verificar se é carregado no botão OK. Se sim, guarda o ficheiro na localização escolhida
                     if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                     {
                         savefilename = saveFileDialog1.FileName;
                     }
-					//Desabilitar o acesso a controlos sobre a aplicação
+		    //Desabilitar o acesso a controlos sobre a aplicação
                     this.ControlBox = false;
-					//Esconder o botão de consolidação
+		    //Esconder o botão de consolidação
                     Button_Limpar.Visible = false;
-					//Fazer aparecer a progress bar
+		    //Fazer aparecer a progress bar
                     progressBarClean.Visible = true;
-					//Tornar visível a label com a atualização do número da etapa em curso
-					label_step.Visible = true;
-					//Atualizar a label com os novos valores das suas propriedades
+		    //Tornar visível a label com a atualização do número da etapa em curso
+		    label_step.Visible = true;
+		    //Atualizar a label com os novos valores das suas propriedades
                     label_step.Refresh();
-					//Ler todos os eventos do ficheiro e guardá-los numa variável do tipo var
+		    //Ler todos os eventos do ficheiro e guardá-los numa variável do tipo var
                     var linesxpto = File.ReadAllLines(@textBox_Ficheiro.Text);
-					//Por cada evento encontrado que possua virgula soma +1 ao valor máximo da progress bar
+		    //Por cada evento encontrado que possua virgula soma +1 ao valor máximo da progress bar
                     foreach (var line_count in linesxpto)
                     {
                         if (line_count.Contains('"'))
@@ -77,7 +77,7 @@ namespace Remove_Comma_Tickets_TXT
                             count_max_prog_bar++;
                         }
                     }
-					//Definir o valor máximo da progress bar
+		    //Definir o valor máximo da progress bar
                     progressBarClean.Maximum = count_max_prog_bar;
 
                     //Remover as aspas de um ficheiro
@@ -96,7 +96,7 @@ namespace Remove_Comma_Tickets_TXT
                             int percent = (int)(((double)(progressBarClean.Value - progressBarClean.Minimum) /
                                     (double)(progressBarClean.Maximum - progressBarClean.Minimum)) * 100);
                             //Atualizar de forma gráfica o preenchimento da progress bar
-							using (Graphics gr = progressBarClean.CreateGraphics())
+			    using (Graphics gr = progressBarClean.CreateGraphics())
                             {
                                 gr.DrawString(percent.ToString() + "%",
                                     SystemFonts.DefaultFont,
@@ -109,12 +109,12 @@ namespace Remove_Comma_Tickets_TXT
 
                             continue;
                         }
-						//Acrescentar novo log
+		        //Acrescentar novo log
                         newFile2.Append(line2 + "\r\n");
                     }
-					//Escrever em ficheiro as novas alterações
+		    //Escrever em ficheiro as novas alterações
                     File.WriteAllText(savefilename, newFile2.ToString());
-					//Fazer reset aos valores da progress bar, mantendo o seu valor máximo anteriormente definido
+		    //Fazer reset aos valores da progress bar, mantendo o seu valor máximo anteriormente definido
                     progressBarClean.Value = 0;
                     progressBarClean.Maximum = 0;
                     progressBarClean.Maximum = count_max_prog_bar;
@@ -137,7 +137,7 @@ namespace Remove_Comma_Tickets_TXT
                             int percent = (int)(((double)(progressBarClean.Value - progressBarClean.Minimum) /
                                     (double)(progressBarClean.Maximum - progressBarClean.Minimum)) * 100);
                             //Atualizar de forma gráfica o preenchimento da progress bar                            
-							using (Graphics gr = progressBarClean.CreateGraphics())
+			    using (Graphics gr = progressBarClean.CreateGraphics())
                             {
                                 gr.DrawString(percent.ToString() + "%",
                                     SystemFonts.DefaultFont,
@@ -166,14 +166,14 @@ namespace Remove_Comma_Tickets_TXT
 					//Fazer reset do valor da progress bar para que esta comece em zero sempre que a aplicação é executada
 					progressBarClean.Value = 0;
                 }
-				//Caso o ficheiro não exista é lançada uma mensagem a avisar o utilizador que o ficheiro selecionado não é válido				
+	        //Caso o ficheiro não exista é lançada uma mensagem a avisar o utilizador que o ficheiro selecionado não é válido				
                 else
                 {
                     MessageBox.Show("O ficheiro introduzido não é válido!!!\nTente novamente!!!");
                 }
             }
         }
-		//Função Click do botão para abrir um ficheiro para processar
+        //Função Click do botão para abrir um ficheiro para processar
         private void textBox_Ficheiro_Click(object sender, EventArgs e)
         {
             //Adicionar qual ficheiro a abrir
@@ -181,7 +181,7 @@ namespace Remove_Comma_Tickets_TXT
             openFileDialog1.InitialDirectory = Convert.ToString(Environment.SpecialFolder.Desktop);
             openFileDialog1.Filter = "Text File (.txt) | *.txt";
             openFileDialog1.FilterIndex = 1;
-			//Validar o ficheiro escolhido, carregando no botão OK
+	    //Validar o ficheiro escolhido, carregando no botão OK
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBox_Ficheiro.Text = openFileDialog1.FileName;
