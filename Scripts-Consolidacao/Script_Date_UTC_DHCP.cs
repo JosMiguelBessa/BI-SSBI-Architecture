78b9933df0f165f0e50cbaa061d021f8b53bf487
José Bessa <jmiguelbessa16@gmail.com>
2017-05-15 16:19:03 +0100

    Script de Consolidação dos logs DHCP

--- Scripts-Consolidacao/Script_Date_UTC_DHCP.cs
+++ Scripts-Consolidacao/Script_Date_UTC_DHCP.cs
@@ -17,40 +17,40 @@ namespace Script_Date_UTC_DHCP
     {
         public Form1()
         {
-			//Método instanciado por defeito utilizado para inicializar a interface gráfica da aplicação
+	    //Método instanciado por defeito utilizado para inicializar a interface gráfica da aplicação
             InitializeComponent();
-			//Esconder progress bar quando arranca o programa
+	    //Esconder progress bar quando arranca o programa
             progressBarClean.Visible = false;
-			//Valor mínimo da progress bar
+	    //Valor mínimo da progress bar
             progressBarClean.Minimum = 0;
-			//Valor adicionado à progress bar em cada iteração
+	    //Valor adicionado à progress bar em cada iteração
             progressBarClean.Step = 1;
         }
 		
-		//Criação de um menu da aplicação
+	//Criação de um menu da aplicação
         private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Esta aplicação é utilizada para eliminar os espaços a mais nos logs DHCP cujo dia é inferior a 10!!!");
         }
 		
-		//Codificação do Botão que efetua a consolidação e transformação dos eventos do ficheiro
+	//Codificação do Botão que efetua a consolidação e transformação dos eventos do ficheiro
         private void buttonLimpar_Click(object sender, EventArgs e)
         {
-			//Variável que irá guardar o caminho para o ficheiro que armazena o resultado do processamento
+	    //Variável que irá guardar o caminho para o ficheiro que armazena o resultado do processamento
             string savefilename = "";
-			//Variável com o valor máximo da progress bar (que equivale ao número de evento a serem processados)
+            //Variável com o valor máximo da progress bar (que equivale ao número de evento a serem processados)
             int count_max_prog_bar = 0;
 
-			//Verificar se foi preenchido o caminho para um ficheiro
+	    //Verificar se foi preenchido o caminho para um ficheiro
             if (string.IsNullOrEmpty(textBoxPathFicheiro.Text))
             {
-				//Apresentar uma mensagem caso não tenha sido preenchido o caminho para um ficheiro
+		//Apresentar uma mensagem caso não tenha sido preenchido o caminho para um ficheiro
                 MessageBox.Show("Path não foi preenchida!!!\nPreencha primeiro a path!!");
             }
-			//Caso tenha sido preenchido o caminho para o ficheiro
+	    //Caso tenha sido preenchido o caminho para o ficheiro
             else
             {
-				//Verificar se o ficheiro existe
+		//Verificar se o ficheiro existe
                 if (File.Exists(textBoxPathFicheiro.Text))
                 {
 
@@ -60,23 +60,23 @@ namespace Script_Date_UTC_DHCP
                     saveFileDialog1.Filter = "Text File (.txt) | *.txt";
                     saveFileDialog1.FilterIndex = 1;
 
-					//Verificar se é carregado no botão OK. Se sim, guarda o ficheiro na localização escolhida
+		    //Verificar se é carregado no botão OK. Se sim, guarda o ficheiro na localização escolhida
                     if (saveFileDialog1.ShowDialog() == DialogResult.OK)
                     {
                         savefilename = saveFileDialog1.FileName;
                     }
 
-					//Desabilitar o acesso a controlos sobre a aplicação
+		    //Desabilitar o acesso a controlos sobre a aplicação
                     this.ControlBox = false;
-					//Esconder o botão de consolidação
+	            //Esconder o botão de consolidação
                     buttonLimpar.Visible = false;
-					//Fazer aparecer a progress bar
+	            //Fazer aparecer a progress bar
                     progressBarClean.Visible = true;
 
-					//Ler todos os eventos do ficheiro e guardá-los numa variável do tipo var
+		    //Ler todos os eventos do ficheiro e guardá-los numa variável do tipo var
                     var linesxpto = File.ReadAllLines(@textBoxPathFicheiro.Text);
 
-					//Por cada evento encontrado que possua uma data, soma +1 ao valor máximo da progress bar
+		    //Por cada evento encontrado que possua uma data, soma +1 ao valor máximo da progress bar
                     foreach (var line_count in linesxpto)
                     {
                         if (line_count.Contains("Jan") || line_count.Contains("Fev") || line_count.Contains("Mar") || line_count.Contains("Apr") || line_count.Contains("May") || line_count.Contains("Jun") || line_count.Contains("Jul") || line_count.Contains("Aug") || line_count.Contains("Sep") || line_count.Contains("Oct") || line_count.Contains("Nov") || line_count.Contains("Dec"))
@@ -86,7 +86,7 @@ namespace Script_Date_UTC_DHCP
                     }
                     progressBarClean.Maximum = count_max_prog_bar;
 					
-					//Substituir dois espaços por apenas um
+		    //Substituir dois espaços por apenas um
                     StringBuilder newFile = new StringBuilder();
                     string temp = "";
                     string[] file = File.ReadAllLines(@textBoxPathFicheiro.Text);
@@ -101,14 +101,14 @@ namespace Script_Date_UTC_DHCP
                         }
                         newFile.Append(line + "\r\n");
                     }
-					//Tentar atualizar progress bar
+		    //Tentar atualizar progress bar
                     try
                     {
 
                         progressBarClean.Increment(1);
                         int percent = (int)(((double)(progressBarClean.Value - progressBarClean.Minimum) /
                                 (double)(progressBarClean.Maximum - progressBarClean.Minimum)) * 100);
-						//Atualizar de forma gráfica o preenchimento da progress bar
+			//Atualizar de forma gráfica o preenchimento da progress bar
                         using (Graphics gr = progressBarClean.CreateGraphics())
                         {
                             gr.DrawString(percent.ToString() + "%",
@@ -120,26 +120,26 @@ namespace Script_Date_UTC_DHCP
                                     SystemFonts.DefaultFont).Height / 2.0F)));
                         }
 						
-						//Escrever em ficheiro as novas alterações desejadas
+			//Escrever em ficheiro as novas alterações desejadas
                         File.WriteAllText(savefilename, newFile.ToString());
-						//Mostrar uma mensagem quando a consolidação do ficheiro estiver concluída
+			//Mostrar uma mensagem quando a consolidação do ficheiro estiver concluída
                         MessageBox.Show("A consolidação e transformação do ficheiro está concluída!!!");
-						//Habilitar novamente os controlos sobre a aplicação
+			//Habilitar novamente os controlos sobre a aplicação
                         this.ControlBox = true;
-						//O botão de consolidação é colocado vísivel
+			//O botão de consolidação é colocado vísivel
                         buttonLimpar.Visible = true;
-						//A progress bar é escondida
+			//A progress bar é escondida
                         progressBarClean.Visible = false;
-						//Fazer reset do valor da progress bar para que esta comece em zero sempre que a aplicação é executada
+			//Fazer reset do valor da progress bar para que esta comece em zero sempre que a aplicação é executada
                         progressBarClean.Value = 0;
                     }
-					//Caso não consiga atualizar a progress bar lança uma exceção para que o utilizador possa perceber qual o problema
+		    //Caso não consiga atualizar a progress bar lança uma exceção para que o utilizador possa perceber qual o problema
                     catch (System.IO.IOException ex)
                     {
                         System.Console.WriteLine(ex.ToString());
                     }
                 }
-				//Caso o ficheiro não exista é lançada uma mensagem a avisar o utilizador que o ficheiro selecionado não é válido
+		//Caso o ficheiro não exista é lançada uma mensagem a avisar o utilizador que o ficheiro selecionado não é válido
                 else
                 {
                     MessageBox.Show("O ficheiro introduzido não é válido!!!\nTente novamente!!!");
@@ -147,7 +147,7 @@ namespace Script_Date_UTC_DHCP
             }
         }
 
-		//Função Click do botão para abrir um ficheiro para processar
+	//Função Click do botão para abrir um ficheiro para processar
         private void textBoxPathFicheiro_Click(object sender, EventArgs e)
         {
             //Adicionar qual ficheiro a abrir
@@ -156,7 +156,7 @@ namespace Script_Date_UTC_DHCP
             openFileDialog1.Filter = "Text File (.txt) | *.txt";
             openFileDialog1.FilterIndex = 1;
 
-			//Validar o ficheiro escolhido, carregando no botão OK
+	    //Validar o ficheiro escolhido, carregando no botão OK
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBoxPathFicheiro.Text = openFileDialog1.FileName;
